0cb20cac95f686084ecb3bb6a3f8811371eac744
Chris Jonathan <christophereko@hotmail.fr>
2019-07-06 13:13:51 +0100

    I've limited the menu's height to 50%, I've made the banner image bigger as well as the blog section and testimonials.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let menuIcon = document.querySelector('.menu-icon');
 let menuDrawer = document.querySelector('.drawer');
+let menuItems = document.querySelector('#mobile-nav ul ');
 
 menuIcon.addEventListener('click', _openDrawer, false);
 
 function _openDrawer () {
     menuDrawer.classList.toggle('open-drawer');
+    menuItems.classList.toggle('reveal-menu-items');
 }
