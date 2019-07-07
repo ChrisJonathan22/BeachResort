876128058ca4d2630797e995be2152344a5ddaa5
Chris Jonathan <christophereko@hotmail.fr>
2019-07-07 09:09:46 +0100

    I've added a border to each menu item with a hover effect to go with it.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let menuIcon = document.querySelector('.menu-icon');
 let menuDrawer = document.querySelector('.drawer');
-let menuItems = document.querySelector('#mobile-nav ul ');
+let menuContainer = document.querySelector('#mobile-nav ul');
+let menuItems = document.querySelectorAll('#mobile-nav ul li');
 
 menuIcon.addEventListener('click', _openDrawer, false);
 
 function _openDrawer () {
     menuDrawer.classList.toggle('open-drawer');
-    menuItems.classList.toggle('reveal-menu-items');
+    menuContainer.classList.toggle('reveal-menu-items');
+    menuItems.forEach(item => {
+        item.classList.toggle('undo-rotate');
+    });
 }
